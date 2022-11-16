6491266b981852affec47dfb3751d049b8e0c284
rjk13637110731 <86515447+rjk13637110731@users.noreply.github.com>
2022-11-16 23:31:47 +0800

    Create Seriablizable_back.java

---
+++ IO/Seriablizable_List/Seriablizable_back.java
@@ -0,0 +1,24 @@
+package com.baidu.javase.chapter23.baidu.com.javase.io;
+
+//反序列化集合
+
+import com.baidu.javase.chapter23.baidu.com.javase.Bean.User;
+
+import java.io.FileInputStream;
+import java.io.ObjectInputStream;
+import java.util.List;
+
+public class ObjectInputStreamTest02 {
+    public static void main(String[] args) throws Exception{
+        ObjectInputStream ois = new ObjectInputStream(new FileInputStream("users"));
+        //Object obj = ois.readObject();
+        //System.out.println(obj instanceof List);
+
+        List<User> userList = (List<User>)ois.readObject();
+        for (User user : userList){
+            System.out.println(user);
+        }
+
+        ois.close();
+    }
+}
