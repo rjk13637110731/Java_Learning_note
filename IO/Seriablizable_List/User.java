bcc32ff1d512d30ba4aab64efadd583fd4dac3ad
rjk13637110731 <86515447+rjk13637110731@users.noreply.github.com>
2022-11-16 23:31:20 +0800

    Create User.java

---
+++ IO/Seriablizable_List/User.java
@@ -0,0 +1,40 @@
+package com.baidu.javase.chapter23.baidu.com.javase.Bean;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+    private int no;
+    private String name;
+
+    public User() {
+    }
+
+    public User(int no, String name) {
+        this.no = no;
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "no=" + no +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    public int getNo() {
+        return no;
+    }
+
+    public void setNo(int no) {
+        this.no = no;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
