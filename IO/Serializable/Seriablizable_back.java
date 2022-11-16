d6efaf4d3da019e409f93532521d4d5cbbbb94c9
rjk13637110731 <86515447+rjk13637110731@users.noreply.github.com>
2022-11-16 23:12:10 +0800

    Create Seriablizable_back.java

---
+++ IO/Serializable/Seriablizable_back.java
@@ -0,0 +1,19 @@
+package com.baidu.javase.chapter23.baidu.com.javase.io;
+
+//反序列化
+
+import java.io.FileInputStream;
+import java.io.ObjectInputStream;
+
+public class ObjectInputStreamTest01{
+    public static void main(String[] args) throws Exception{
+        ObjectInputStream ois = new ObjectInputStream(new FileInputStream("students"));
+
+        //开始反序列化，读
+        Object obj = ois.readObject();
+
+        //反序列化回来是一个学生对象，所以会调用学生对象的toString()方法。
+        System.out.println(obj);
+        ois.close();
+    }
+}
