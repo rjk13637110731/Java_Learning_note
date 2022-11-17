be1a114e256f42f18b7988c17f9b1de8145f496a
rjk13637110731 <86515447+rjk13637110731@users.noreply.github.com>
2022-11-17 09:31:01 +0800

    Create Serializable_front.java

---
+++ IO/Serializable_change/Serializable_front.java
@@ -0,0 +1,33 @@
+//序列化
+package com.baidu.javase.chapter23.baidu.com.javase.io;
+
+import com.baidu.javase.chapter23.baidu.com.javase.Bean.Student;
+
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
+
+/*
+java.io.NotSerializableException
+    Student对象不支持序列化！
+
+    参与序列化和反序列化的对象，必须实现Serializable接口。
+ */
+
+public class ObjectOutputStreamTest01 {
+    public static void main(String[] args) throws Exception{
+        //创建Java对象
+        Student s = new Student(1111,"zhangsan");
+
+        //序列化
+        ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("students"));
+
+        //序列化对象
+        oos.writeObject(s);
+
+        //刷新
+        oos.flush();
+
+        //关闭
+        oos.close();
+    }
+}
