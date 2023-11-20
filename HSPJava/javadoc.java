64112186a96eedf430b207201b5151e3fd2a4922
TangJ <86515447+rjk13637110731@users.noreply.github.com>
2023-11-20 14:40:33 +0800

    Create javadoc.java

---
+++ HSPJava/javadoc.java
@@ -0,0 +1,24 @@
+/**
+ * @author 小明
+ * @version 1.0
+*/
+public class Comments{
+	public static void main(String[] args){
+		System.out.println("书名\t作者\t价格\t销量\n三国\t罗贯中\t120\t1000");
+
+		/* 示例
+		   下面代码完成：两个数相加
+		   定义变量
+
+		   注释：
+		   int n1 = 10;....
+	    */
+
+		int n1 = 10;
+		int n2 = 30;
+		// 求和
+		int sum = n1+n2;
+		// 输出结果
+		System.out.println("结果 = "+ sum);
+	}
+}
