ada49ae5315c7fe08d8e57aae6b7221be2dcf590
TangJ <86515447+rjk13637110731@users.noreply.github.com>
2023-12-05 23:52:51 +0800

    Create compareTo-float-double.java

---
+++ HSPJava/chapter03/compareTo-float-double.java
@@ -0,0 +1,8 @@
+public class Main{
+    public static void main(String[] args){
+        double num9 = 2.1234567851;
+        float num10 = 2.1234567851F;
+        System.out.println(num9);
+        System.out.println(num10);
+    }
+}
