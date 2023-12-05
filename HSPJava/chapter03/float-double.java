d8ff58b1671b53cacfd5ea27f694e384ea3a75e1
TangJ <86515447+rjk13637110731@users.noreply.github.com>
2023-12-05 23:47:35 +0800

    Create float-double.java

---
+++ HSPJava/chapter03/float-double.java
@@ -0,0 +1,8 @@
+public class Main{
+    public static void main(String[] args){
+        double d1 = 3.14;
+        float f1 = 2.236f;
+        System.out.println(f1);
+        System.out.println(d1);
+    }
+}
