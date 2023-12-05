e28d817fd2258235a3190b59a53b1db153645ddb
TangJ <86515447+rjk13637110731@users.noreply.github.com>
2023-12-05 23:53:09 +0800

    Create trap-float-double.java

---
+++ HSPJava/chapter03/trap-float-double.java
@@ -0,0 +1,8 @@
+public class Main{
+    public static void main(String[] args){
+        double num11 = 2.7;
+        double num12 = 8.1 / 3;
+        System.out.println(num11);
+        System.out.println(num12); // 接近2.7的一个小数，而不是2.7
+    }
+}
