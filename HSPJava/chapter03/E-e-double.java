6949f41ab273ef438ef91cde7f337a31e004d49f
TangJ <86515447+rjk13637110731@users.noreply.github.com>
2023-12-05 23:48:00 +0800

    Create E-e-double.java

---
+++ HSPJava/chapter03/E-e-double.java
@@ -0,0 +1,11 @@
+public class Main{
+    public static void main(String[] args){
+        double num5 = 0.123;
+        double num6 = .123; // 等价于0.123
+        System.out.println(num5);
+        System.out.println(num6);
+
+        System.out.println(5.12e2);
+        System.out.println(5.12e-2);
+    }
+}
