7dd54341ae81b01507f2599c9935b0c4a3f71bb6
TangJ <86515447+rjk13637110731@users.noreply.github.com>
2023-12-22 00:08:06 +0800

    Create if-else-exc.java

---
+++ HSPJava/chapter05/if-else-exc.java
@@ -0,0 +1,14 @@
+public class Main{
+    public static void main(String[] args){
+        int x = 7;
+        int y = 4;
+        if (x > 5){
+            if (y > 5){
+                System.out.println(x + y);
+            }
+            System.out.println("韩顺平教育~");
+        }else{
+            System.out.println("x is " + x);
+        }
+    }
+}
