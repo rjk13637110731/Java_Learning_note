0f082ff84217f6d3e79eb1ae5850a76791065baf
TangJ <86515447+rjk13637110731@users.noreply.github.com>
2023-12-22 20:17:59 +0800

    Create for-3.java

---
+++ HSPJava/chapter05/for-3.java
@@ -0,0 +1,8 @@
+public class Main{
+    public static void main(String[] args){
+        int count = 3;
+        for(int i = 0,j = 0;i < count;i++,j+=2){
+            System.out.println("i = " + i + " j = " + j);
+        }
+    }
+}
