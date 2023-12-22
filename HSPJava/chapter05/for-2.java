f5f83a01d54c80804879c95a1a75109fc5bfe748
TangJ <86515447+rjk13637110731@users.noreply.github.com>
2023-12-22 20:17:44 +0800

    Create for-2.java

---
+++ HSPJava/chapter05/for-2.java
@@ -0,0 +1,10 @@
+public class Main{
+    public static void main(String[] args){
+        int i = 1; // 循环变量初始化
+        for(;i <= 10;){
+            System.out.println("你好，韩顺平教育!");
+            i++; // 循环变量迭代
+        }
+        System.out.println("i = " + i); // ok
+    }
+}
