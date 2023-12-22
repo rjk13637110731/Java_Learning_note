9cc2d65179a90264ec2148fd94adb9058edd80d8
TangJ <86515447+rjk13637110731@users.noreply.github.com>
2023-12-22 21:08:04 +0800

    Create while-exc.java

---
+++ HSPJava/chapter05/while-exc.java
@@ -0,0 +1,12 @@
+public class Main{
+    public static void main(String[] args){
+        int i = 1;
+        int end = 100;
+        while(i <= end){
+            if(i % 3 == 0){
+                System.out.println("i = " + i);
+            }
+            i++;
+        }
+    }
+}
