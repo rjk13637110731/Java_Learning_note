a1ce8c3d220676878afddaefdcc4c98be532a230
TangJ <86515447+rjk13637110731@users.noreply.github.com>
2023-12-22 21:08:19 +0800

    Create while-exc-2.java

---
+++ HSPJava/chapter05/while-exc-2.java
@@ -0,0 +1,12 @@
+public class Main{
+    public static void main(String[] args){
+        int i = 40;
+        int end = 200;
+        while(i <= end){
+            if(i % 2 == 0){
+                System.out.println("i = " + i);
+            }
+            i++;
+        }
+    }
+}
