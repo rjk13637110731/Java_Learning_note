1740cef3d135445a3018009217318a34dcc6812a
TangJ <86515447+rjk13637110731@users.noreply.github.com>
2023-12-23 22:43:37 +0800

    Create mul-for-while-1.java

---
+++ HSPJava/chapter05/mul-for-while-1.java
@@ -0,0 +1,9 @@
+public class Main{
+    public static void main(String[] args){
+        for(int i = 1;i <= 7;i++){ // 第一层循环 7
+            for(int j = 1;j <= 2;j++){ // 第二层循环 2
+                System.out.println("ok~~"); // 7 * 2 = 14
+            }
+        }
+    }
+}
